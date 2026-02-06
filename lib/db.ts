c305c8fd59b53ccc3bd7de2bf5036768aec9e317
NonPro <non3518@hotmail.com>
2026-02-06 17:30:42 +0700

    Fix SSL

--- lib/db.ts
+++ lib/db.ts
@@ -6,6 +6,7 @@ const pool = new Pool({
   host: process.env.DB_HOST || "localhost",
   port: parseInt(process.env.DB_PORT || "5432"),
   database: process.env.DB_NAME || "inventory_db",
+  ssl: process.env.DB_SSLMODE === "require" ? { rejectUnauthorized: false } : false,
   user: process.env.DB_USER || "postgres",
   password: process.env.DB_PASSWORD,
   max: 20,
